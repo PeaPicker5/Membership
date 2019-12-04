5b664d0ef609e74011edf1e66e1ae6ad65e52048
Kevin Doran <Kevin.Doran@go-mavin.com>
2019-12-04 09:10:39 -0500

    Added XAML usage to IntegerGreaterThanAmount Converter

--- Membership/Common/Converters/IntegerGreaterThanAmount.cs
+++ Membership/Common/Converters/IntegerGreaterThanAmount.cs
@@ -5,6 +5,24 @@ using System.Windows.Data;
 namespace Membership.Common.Converters
 {
     [ValueConversion(typeof(int), typeof(bool))]
+
+    // XAML USAGE
+    //
+    //<DataGrid.Resources>
+    //  <Style x:Key="CheckBoxStyle" TargetType="{x:Type CheckBox}" >
+    //      <Style.Triggers>
+    //          <DataTrigger Binding = "{Binding Path=Year,Converter={StaticResource IntegerGreaterThanAmount}}"
+    //                                      Value="True">
+    //               <Setter Property = "IsEnabled" Value="False"/>
+    //          </DataTrigger>
+    //      </Style.Triggers>
+    //   </Style>
+    //</DataGrid.Resources>-->
+
+    //  <DataGridCheckBoxColumn MinWidth = "40" Header="PAID" Binding="{Binding Path=IsPaid}" 
+    //                          ElementStyle="{StaticResource CheckBoxStyle}" />
+
+
     public class IntegerGreaterThanAmount : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
